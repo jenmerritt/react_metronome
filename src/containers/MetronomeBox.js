d95035f8be100b41577b454ceb53326f3db2c7e1
Jen Merritt <jenjomez@gmail.com>
2020-03-04 11:34:10 +0000

    sound playing on loop - sorry Hannah :D

--- src/containers/MetronomeBox.js
+++ src/containers/MetronomeBox.js
@@ -7,27 +7,33 @@ class MetronomeBox extends Component{
   constructor(props){
     super(props);
     this.state = {
-      speed: 40,
+      speed: 1000,
       sound: {
         title: "tick",
         filePath: '/sounds/tick.wav'
       }
     }
     this.playSound = this.playSound.bind(this)
+    this.loopAudio = this.loopAudio.bind(this)
   }
 
   playSound(){
     const audioFile = new Audio(this.state.sound.filePath)
-    audioFile.play();
+      audioFile.play()
+  }
+
+  loopAudio(){
+    const myTimer = setInterval(this.playSound, this.state.speed);
   }
 
   render(){
+
     return(
       <section>
         <h1>Metronome</h1>
         <p>Speed: {this.state.speed}</p>
         <p>Sound: {this.state.sound.title}</p>
-        <button onClick={this.playSound}>PLAY!</button>
+        <button onClick={this.loopAudio}>PLAY!</button>
         <SpeedSelector speed={this.state.speed} />
         <SoundSelector sound={this.state.sound} />
       </section>
