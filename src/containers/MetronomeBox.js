d36c73a896331031b9ae1368222813e7bda33d01
Jen Merritt <jenjomez@gmail.com>
2020-03-04 13:24:56 +0000

    metronome will stop

--- src/containers/MetronomeBox.js
+++ src/containers/MetronomeBox.js
@@ -10,30 +10,40 @@ class MetronomeBox extends Component{
       speed: 1000,
       sound: {
         title: "tick",
-        filePath: '/sounds/tick.wav'
-      }
+        audio: new Audio('/sounds/tick.wav')
+      },
+      myTimer: null
     }
     this.playSound = this.playSound.bind(this)
     this.loopAudio = this.loopAudio.bind(this)
+    this.clearAudio = this.clearAudio.bind(this)
+  }
+
+  loopAudio(){
+      this.setState({
+        myTimer: setInterval(this.playSound, this.state.speed)
+        });
+  }
+
+  clearAudio(){
+    clearInterval(this.state.myTimer)
   }
 
   playSound(){
     const audioFile = new Audio(this.state.sound.filePath)
-      audioFile.play()
-  }
-
-  loopAudio(){
-    const myTimer = setInterval(this.playSound, this.state.speed);
+    this.state.sound.audio.play()
   }
 
   render(){
 
     return(
+
       <section>
         <h1>Metronome</h1>
         <p>Speed: {this.state.speed}</p>
         <p>Sound: {this.state.sound.title}</p>
         <button onClick={this.loopAudio}>PLAY!</button>
+        <button onClick={this.clearAudio}>HANNAH!</button>
         <SpeedSelector speed={this.state.speed} />
         <SoundSelector sound={this.state.sound} />
       </section>
