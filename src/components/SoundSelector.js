5636152d41b062bee50998dd2bc8e2bd824e8ae8
Jen Merritt <jenjomez@gmail.com>
2020-03-04 14:16:54 +0000

    select multiple sounds

--- src/components/SoundSelector.js
+++ src/components/SoundSelector.js
@@ -1,8 +1,21 @@
 import React from 'react';
 
 function SoundSelector(props){
+
+  function handleChange(event){
+    props.onSoundSelected(event.target.value);
+}
+
   return(
-    null
+    <select defaultValue="default" onChange={handleChange}>
+      <option disabled value="default">Choose a sound...</option>
+      <option value="/sounds/countdown.mp3">Countdown</option>
+      <option value="/sounds/tick.wav">Tick</option>
+      <option value="/sounds/dog.mp3">Dog</option>
+      <option value="/sounds/monstermunch.mp3">Monster Munch</option>
+      <option value="/sounds/meow.mp3">Meow</option>
+      <option value="/sounds/jelly.mp3">Jelly</option>
+    </select>
   )
 }
 
--- src/containers/MetronomeBox.js
+++ src/containers/MetronomeBox.js
@@ -8,22 +8,27 @@ class MetronomeBox extends Component{
     super(props);
     this.state = {
       speed: 200,
-      sound: {
-        title: "tick",
-        audio: new Audio('/sounds/tick.wav')
-      },
+      sound: new Audio('/sounds/tick.wav'),
       myTimer: null
     }
     this.playSound = this.playSound.bind(this)
     this.loopAudio = this.loopAudio.bind(this)
     this.clearAudio = this.clearAudio.bind(this)
     this.handleSpeedSelected = this.handleSpeedSelected.bind(this)
-
+    this.handleSoundSelected = this.handleSoundSelected.bind(this)
   }
 
   handleSpeedSelected(newSpeed){
     this.setState({
       speed: newSpeed
+    })
+    this.clearAudio()
+    this.loopAudio()
+  }
+
+  handleSoundSelected(newSound){
+    this.setState({
+      sound: new Audio(newSound)
     })
   }
 
@@ -38,8 +43,8 @@ class MetronomeBox extends Component{
   }
 
   playSound(){
-    const audioFile = new Audio(this.state.sound.filePath)
-    this.state.sound.audio.play()
+    const audioFile = new Audio(this.state.sound)
+    this.state.sound.play()
   }
 
   render(){
@@ -49,11 +54,10 @@ class MetronomeBox extends Component{
       <section>
         <h1>Metronome</h1>
         <p>Speed: {this.state.speed}</p>
-        <p>Sound: {this.state.sound.title}</p>
         <button onClick={this.loopAudio}>PLAY!</button>
         <button onClick={this.clearAudio}>HANNAH!</button>
         <SpeedSelector speed={this.state.speed}  onSpeedSelected={this.handleSpeedSelected}/>
-        <SoundSelector sound={this.state.sound}  />
+        <SoundSelector sound={this.state.sound}  onSoundSelected={this.handleSoundSelected}/>
       </section>
     )
   }
